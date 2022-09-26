fbde178dadaaae6e8baaffbd9f421ece6a71057e
Maycol1490 <maicol1490@hotmail.com>
2022-09-25 20:46:24 -0500

    LOADING

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -7,7 +7,7 @@ const Loading = () => {
             <div className='container_loading'>
                 <img src={imgLoading} alt="Cargando" />
                 <h3 className='spinner'>
-                    <span>P</span> <span>R</span> <span>O</span> <span>G</span> <span>N</span> <span>O</span> <span>S</span> <span>T</span> <span>I</span> <span>C</span> <span>A</span> <span>T</span> <span>E</span> <span>.</span> <span>.</span> <span>.</span>
+                    <span>L</span> <span>O</span> <span>A</span> <span>D</span> <span>I</span> <span>N</span> <span>G</span> <span>.</span> <span>.</span> <span>.</span>
                 </h3>
             </div>
         </div>
